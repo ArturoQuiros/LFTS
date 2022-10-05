9495c3eb13e8564daee160418cfede1e3a3a07d9
Arturo Quirós <joseq2408@gmail.com>
2022-10-04 21:18:44 -0600

    04

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,19 +8,19 @@
 <body>
     <article>
 
-        <h1> <a href="/post"> My First Post</a></h1>
+        <h1> <a href="/posts/my-first-post"> My First Post</a></h1>
         <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta alias sit dolore, recusandae harum, itaque eaque mollitia consequuntur molestiae reprehenderit iure. Quas ipsa aliquid rerum porro, asperiores veritatis. Corporis, alias.</p>
 
     </article>
     <article>
 
-        <h1> <a href="/post">  My Second Post </a></h1>
+        <h1> <a href="/posts/my-second-post">  My Second Post </a></h1>
         <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta alias sit dolore, recusandae harum, itaque eaque mollitia consequuntur molestiae reprehenderit iure. Quas ipsa aliquid rerum porro, asperiores veritatis. Corporis, alias.</p>
 
     </article>
     <article>
 
-        <h1> <a href="/post">  My Third Post</a></h1>
+        <h1> <a href="/posts/my-third-post">  My Third Post</a></h1>
         <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta alias sit dolore, recusandae harum, itaque eaque mollitia consequuntur molestiae reprehenderit iure. Quas ipsa aliquid rerum porro, asperiores veritatis. Corporis, alias.</p>
 
     </article>
