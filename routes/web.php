09fa606f788c380a851bac9c3cd248a8fe1bf562
Arturo Quirós <joseq2408@gmail.com>
2022-10-27 19:32:06 -0600

    101

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,24 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us14'
+    ]);
+
+    //$response = $mailchimp->lists->getAllLists();
+    // $response = $mailchimp->lists->getList('93659e24b4');
+    // $response = $mailchimp->lists->getListMembersInfo('93659e24b4');
+    $response = $mailchimp->lists->addListMember('93659e24b4', [
+        'email_address' => 'tommy@gmail.com',
+        'status' => 'subscribed'
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('post'); //where, or whereAlpha, whereNumber, whereAlphaNumeric
 Route::post('/posts/{post:slug}/comments', [CommentController::class, 'store']);
